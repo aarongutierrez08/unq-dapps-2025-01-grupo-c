4038a58099cb1f262d003800d0ebf0c4ea79cc16
agutierrez <agutierrez@zetti.com.ar>
2025-04-13 17:23:39 -0300

    Example controller added, build.gradle.kts and ci-cd.yml updated.

---
+++ src/test/DemoApplication.kt
@@ -0,0 +1,22 @@
+my-spring-boot-app/
+├── build.gradle.kts
+├── settings.gradle.kts
+├── src/
+│   ├── main/
+│   │   ├── kotlin/
+│   │   │   └── com/example/demo/  # Paquete base
+│   │   │       ├── DemoApplication.kt
+│   │   └── resources/
+│   │       ├── application.yml
+│   └── test/
+│       ├── kotlin/
+│       │   └── com/example/demo/
+│       │       ├── DemoApplicationTests.kt
+│       └── resources/
+├── gradle/
+│   └── wrapper/
+│       ├── gradle-wrapper.jar
+│       └── gradle-wrapper.properties
+├── gradlew
+├── gradlew.bat
+└── README.md
