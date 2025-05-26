65c8502a1fc51ed4752705e5cea6dca975f13430
agutierrez <agutierrez@zetti.com.ar>
2025-05-25 23:33:03 -0300

    Dockerfile new chrome version

--- src/main/kotlin/com/example/unq_dapps_2025_01_grupo_c/service/WhoScoredService.kt
+++ src/main/kotlin/com/example/unq_dapps_2025_01_grupo_c/service/WhoScoredService.kt
@@ -27,7 +27,8 @@ class WhoScoredService {
                 "--no-sandbox",
                 "--disable-dev-shm-usage",
                 "--disable-gpu",
-                "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+                "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+                "--remote-debugging-port=9222"
             )
             setPageLoadStrategy(PageLoadStrategy.NORMAL)
             setBinary(System.getenv("CHROME_BIN") ?: "/usr/bin/google-chrome")
